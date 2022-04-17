17f91b61f0d6b0253b609b909c52f418f8dd9940
shkim1026 <60453195+shkim1026@users.noreply.github.com>
2022-04-16 22:58:37 -0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$('.burger-button').click(() => {
+    $('.burger-button').hide();
+    $('.burger-button--close').show();
+    $('.header__nav--mobile_container').show();
+    $('.header__nav--mobile').show();
+    $('.header__pages--mobile').addClass('slide-out').removeClass('slide-in');
+});
+
+$('.burger-button--close, .header__nav--mobile_container').click(() => {
+    $('.header__pages--mobile').addClass('slide-in').removeClass('slide-out');
+    $('.burger-button').show();
+    $('.burger-button--close').hide();
+    setTimeout(hideBlkBackground, 500)
+    function hideBlkBackground() {
+        $('.header__nav--mobile_container').hide();
+    }
+});
+
+$('.header__pages--mobile').click((event) => {
+    event.stopPropagation();
+});
